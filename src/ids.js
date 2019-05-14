35f0d118fddf273f5c86c269ea84a54beeb6af2a
Sandy Wyper <sandywyper@gmail.com>
2019-05-14 11:27:21 +0100

    much added

--- src/ids.js
+++ src/ids.js
@@ -2,7 +2,8 @@ const ids = {
 	hammer: "Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0LzM1NDU5ODQ0MDE0OTI=",
 	mandolin: "Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0LzM1NDc0OTkxMDIyOTI=",
 	musicCollection: "Z2lkOi8vc2hvcGlmeS9Db2xsZWN0aW9uLzEyOTQ3NjY5MDAwNA==",
-	xmasToy: "Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0LzM1NDYwMTI4NzY4ODQ="
+	xmasToy: "Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0LzM1NDYwMTI4NzY4ODQ=",
+	buoy: "Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0LzI4MTAzMDM5ODExNjY4"
 };
 
 export default { ids };
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 
 import Client from "shopify-buy";
 import ids from "./ids";
+import displayThisItem from "./displayItem";
 
 const idTags = ids.ids;
 
@@ -10,43 +11,70 @@ const client = Client.buildClient({
   storefrontAccessToken: "8b066968ace6c0f3399eae24531d0293"
 });
 
-function readyPage() {
-  // console.log(window.location.pathname === "/");
+const checkoutId = localStorage.getItem("sandy-shop-checkoutId");
 
-  if (window.location.pathname === "/") {
-    // document.querySelector(".wrapper").addEventListener("click", handleClick);
-
-    // sets listeners for buttons requiring js actions
-    // document.querySelector("#open-menu").addEventListener("click", openSideMenu);
-    // document.querySelector("#close-menu").addEventListener("click", closeSideMenu);
-
-    displayFeaturedCollection(idTags.musicCollection);
-    fetchFeaturedItem(idTags.xmasToy);
-  } else if (window.location.pathname.substring(1, 8) === "product") {
-    fetchProductInfo();
-  } else if (window.location.pathname.substring(1, 11) === "collection") {
-    console.log("yup, this is a collection page");
+function checkCart() {
+  if (checkoutId === null) {
+    client.checkout
+      .create()
+      .then(function(cart) {
+        //do something with cart
+        localStorage.setItem("sandy-shop-checkoutId", cart.id); // Store the ID in localStorage
+      })
+      .catch(err => console.log("problem making cart", err));
+  } else {
+    client.checkout
+      .fetch(checkoutId)
+      .then(function(cart) {
+        // console.log(cart);
+        populateCartMenu(cart);
+        // do something with cart
+      })
+      .catch(err => console.log("problem fetching old cart", err));
   }
 }
 
-// Fetch all products in your shop
-// client.product.fetchAll().then((products) => {
-//     // Do something with the products
-//     console.log(products);
-// }).catch( (err) => console.log(err));
+function readyPage() {
+  //  localStorage.removeItem('sandy-shop-checkoutId');
+  checkCart();
+  document.querySelector("#open-cart").addEventListener("click", openCart);
+  document.querySelector("#close-cart").addEventListener("click", closeCart);
 
-function openSideMenu() {
-  // reveal the side menu.
-  document.querySelector("#side-menu").style.width = "150px";
-  document.querySelector("#wrapper").style.marginRight = "150px";
+  document.querySelector("body").addEventListener("click", handleClick);
+  
+  if (window.location.pathname === "/") {
+    // sets listeners for buttons requiring js actions
+    
+    displayFeaturedCollection(idTags.musicCollection);
+    fetchFeaturedItem(idTags.xmasToy);
+  } else if (window.location.pathname.substring(1, 8) === "product") {
+   
+    fetchProductInfo();
+  } else if (window.location.pathname.substring(1, 11) === "collection") {
+   
+    console.log("yup, this is a collection page");
+  }
+  // Fetch all products in your shop
+  client.product
+    .fetchAll()
+    .then(products => {
+      // Do something with the products
+      // console.log(products);
+    })
+    .catch(err => console.log(err));
 }
 
-function closeSideMenu() {
+function openCart() {
+  // reveal the side menu.
+  document.querySelector("#side-menu").style.width = "400px";
+  // document.querySelector("#body").style.left = "400px";
+}
+
+function closeCart() {
   // close the side menu
   document.querySelector("#side-menu").style.width = "0";
-  document.querySelector("#wrapper").style.marginRight = "0";
+  // document.querySelector("#body").style.marginRight = "0";
 }
-
 
 function displayFeaturedCollection(id) {
   // Fetch a single collection by ID, including its products
@@ -111,7 +139,19 @@ function fetchFeaturedItem(id) {
     .catch(err => console.log(err));
 }
 
+function getVariantBuyUrl(id) {
+  // Fetch a single product by ID
+
+  client.product
+    .fetch(id)
+    .then(product => {
+      console.log(product);
+    })
+    .catch(err => console.log(err));
+}
+
 function displayFeaturedItem(item) {
+  // console.log(item);
   let imageSrc = [];
   if (item.images) {
     let images = item.images;
@@ -125,6 +165,14 @@ function displayFeaturedItem(item) {
   itemVariants.forEach(function(x) {
     itemPrice.push(x.price);
   });
+
+  // get the first variants id
+  let firstVariant = item.variants[0];
+  let variantId = firstVariant.id;
+
+  //get variant for buy-now url
+  // const buyUrl = getVariantBuyUrl(variantId);
+  // console.log(firstVariant.id);
 
   let itemSection = document.querySelector("#featured-item");
 
@@ -142,8 +190,8 @@ function displayFeaturedItem(item) {
         <p>________</p>
       </div>
       <div class="buy-buttons">
-        <button class="btn1">ADD TO CART</button>
-        <button class="btn2">BUY NOW</button>
+        <button class="btn1 add-to-cart" data-id="${variantId}">ADD TO CART</button>
+        <button class="btn2 buy-now" data-id="${variantId}">BUY NOW</button>
       </div>
       <div>
         <p class="full-details item-link"><a href="./product/${
@@ -161,18 +209,6 @@ function displayFeaturedItem(item) {
   `;
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
 function fetchProductInfo() {
   let itemSection = document.querySelector("#item-section");
   let productId = itemSection.dataset.id;
@@ -186,54 +222,172 @@ function fetchProductInfo() {
     .catch(err => console.log("fetching error", err));
 }
 
-function displayThisItem(item) {
-  let imageSrc = [];
+// function displayThisItem(item) {
+//   let imageSrc = [];
+// // console.log(item);
 
-  // fetch item's images
-  if (item.images) {
-    let images = item.images;
-    images.forEach(function(i) {
-      imageSrc.push(i.src);
-    });
+// // fetch item's images
+//   if (item.images) {
+//     let images = item.images;
+//     images.forEach(function(i) {
+//       imageSrc.push(i.src);
+//     });
+//   }
+
+//   //retrieve item price
+//   let itemPrice = [];
+//   let itemVariants = item.variants;
+//   itemVariants.forEach(function(x) {
+//     itemPrice.push(x.price);
+//   });
+
+//     //TODO: handle cases with multiple variants
+//     let firstVariant = item.variants[0];
+//     let variantId = firstVariant.id;
+//   let itemSection = document.querySelector("#item-section");
+
+//   itemSection.innerHTML += `
+//         <div class="product-image">
+//           ${ item.images.map( image => {
+//             // console.log( image )
+//             return `<img src="${image.src}" alt="${image.altText}">`
+//           } )}
+//         </div>
+//         <div class="product-details">
+//           <div class="product-price">
+//             <h1>${item.title}</h1>
+//             <h3 class="price">&pound;${itemPrice[0]}</h3>
+//             <p class="tax">Tax Included.</p>
+//             <p>________</p>
+//           </div>
+//           <div class="buy-buttons">
+//           <button class="btn1 add-to-cart" data-id="${variantId}">ADD TO CART</button>
+//             <button class="btn2">BUY NOW</button>
+//           </div>
+//           <div>
+//             <div class="product-description">
+//               ${item.descriptionHtml}
+//             </div>
+//             <div class="social">
+//               <p><i class="fab fa-facebook-f"></i> Share<p>
+//                     &nbsp;
+//               <p><i class="fab fa-twitter"></i> Tweet</p>
+//                     &nbsp;
+//               <p><i class="fab fa-pinterest-p"></i> Pin it</p>
+//             </div>
+//           </div>
+//         </div>
+//       `;
+// }
+
+function handleClick(event) {
+  // if the click target has a class of 'item-link' then extract id of that item and pass to next func.
+  if (event.target.classList.contains("add-to-cart")) {
+    let buttonClicked = event.target;
+    let id = buttonClicked.dataset.id;
+
+    addItemToCart(id);
+  } else if (event.target.classList.contains("buy-now")) {
+    let buttonClicked = event.target;
+    let id = buttonClicked.dataset.id;
+
+    console.log("click");
+    addItemAndCheckout(id);
+  } else if (event.target.classList.contains("less")) {
+    let buttonClicked = event.target;
+    let id = buttonClicked.dataset.id;
+    console.log("click");
+    removeItemFromCart(id);
   }
+}
 
-  //retrieve item price
-  let itemPrice = [];
-  let itemVariants = item.variants;
-  itemVariants.forEach(function(x) {
-    itemPrice.push(x.price);
-  });
-  let itemSection = document.querySelector("#item-section");
+function removeItemFromCart(id) {
+  const lineItemIdsToRemove = [id];
 
-  itemSection.innerHTML += `
-        <div class="product-image">
-          <img src="${imageSrc[0]}" alt="${item.title}">
+  // Remove an item from the checkout
+  client.checkout
+    .removeLineItems(checkoutId, lineItemIdsToRemove)
+    .then(checkout => {
+      // Do something with the updated checkout
+      populateCartMenu(checkout);
+      // console.log(checkout.lineItems);
+    });
+}
+function addItemAndCheckout(id) {
+  const lineItems = {
+    variantId: id,
+    quantity: 1
+  };
+  client.checkout
+    .addLineItems(checkoutId, lineItems)
+    .then(checkout => {
+      console.log(checkout.webUrl);
+      window.location.href = `${checkout.webUrl}`;
+    })
+    .catch(err => console.log("problem adding item to cart", err));
+}
+
+
+
+function addItemToCart(id) {
+
+  const lineItems = {
+    variantId: id,
+    quantity: 1
+  };
+
+
+  client.checkout
+    .addLineItems(checkoutId, lineItems)
+    .then(checkout => {
+      // debugger;
+      console.log(checkout); // Checkout with two additional line items
+      // console.log(checkout.lineItems); // Line items on the checkout
+    })
+    .catch(err => console.log("problem adding item to cart", err));
+
+      openCart();
+}
+// addItemToCart(id);}
+
+function populateCartMenu(cart) {
+  let lineItems = cart.lineItems;
+  let cartField = document.querySelector("#cart-contents");
+
+  if (lineItems.length < 1) {
+    cartField.innerHTML = `
+    <p>Your cart is currently empty.</p>
+    `;
+  } else {
+    cartField.innerHTML = `
+    ${lineItems.map(i => {
+      let eachItemImage = i.variant.image;
+      let eachPrice = i.variant.price;
+
+      return `
+      <div class="cart-item">
+        <img src="${eachItemImage.src}" alt="cart-item-image">
+        <div class="title-quantity">
+          <h5>${i.title}</h5>
+          <div class="item-price-total">
+            <p>Quantity : ${i.quantity}</p>
+            <p class="less" data-id="${i.id}">remove</p>
+            <p><strong>&pound;${eachPrice * i.quantity}</strong></p>
+          </div>
         </div>
-        <div class="product-details">
-          <div class="product-price">
-            <h1>${item.title}</h1>
-            <h3 class="price">&pound;${itemPrice[0]}</h3>
-            <p class="tax">Tax Included.</p>
-            <p>________</p>
-          </div>
-          <div class="buy-buttons">
-            <button class="btn1">ADD TO CART</button>
-            <button class="btn2">BUY NOW</button>
-          </div>
-          <div>
-            <div class="product-description">
-              ${item.descriptionHtml}
-            </div>
-            <div class="social">
-              <p><i class="fab fa-facebook-f"></i> Share<p>
-                    &nbsp;
-              <p><i class="fab fa-twitter"></i> Tweet</p>
-                    &nbsp;
-              <p><i class="fab fa-pinterest-p"></i> Pin it</p>
-            </div>
-          </div>
-        </div>
+      </div>
       `;
+    })}
+    <div class="go-to-checkout">
+      <div class="subtotal">
+        <h3>subtotal</h3>
+        <h3>&pound;${cart.subtotalPrice}</h3>
+      </div>
+      <p>Tax included and shipping calculated at checkout</p>
+      <button>checkout &rarr;</button>
+    </div> 
+    `;
+  }
 }
 
 window.onload = readyPage();
